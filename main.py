c98c18326f3c63af4329f5cd843356ffddfba7fb
Gabriel Lira <gdossantoslira@gmail.com>
2024-05-25 17:35:27 -0300

    JSON
    Enviar os dados dos cadastros de  requisitante para o JSON

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template,request,redirect,flash,session
+import json
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'PI01'
 
-
+#caminhos definidos para as páginas
 @app.route("/")
+def cadastro():
+
+    return render_template('cadastro.html')
+
+
+@app.route('/login')
 def index():
 
     return render_template('index.html')
-
 
 
 @app.route('/loginrequisitante')
@@ -18,6 +24,17 @@ def loginrequisitante():
     return render_template('loginrequisitante.html')
 
 
+@app.route('/loginoperador')
+def loginoperador():
+
+    return render_template('loginoperador.html')
+
+@app.route('/layout')
+def layout():
+
+    return render_template('layout.html')
+
+#verificação de usuário e senha para requisitante
 @app.route("/acessorequisitante", methods=['POST'])
 def acessorequisitante():
     usuario = request.form.get('usuariorequisitante')
@@ -28,16 +45,45 @@ def acessorequisitante():
     else:
         return redirect('/loginrequisitante')
 
+#verificação de usuário e senha para operador 
 
-@app.route('/layout')
-def layout():
+@app.route("/acessooperador", methods=['POST'])
+def acessooperador():
+    usuario = request.form.get('usuariooperador')
+    senha = request.form.get('senhaoperador')
 
-    return render_template('layout.html')
+    if usuario == 'grupo10' and senha == '123':
+        return redirect('/layout')
+    else:
+        return redirect('/loginoperador')
+    
+#Enviar os dados dos cadastros de  requisitante para o JSON
 
 
+@app.route("/requisitantecadastro", methods=['POST'])
+def requisitantecadastro():
 
+    email = request.form.get('emailrequisitante')
+    nome = request.form.get('nomerequisitante')
+    senha = request.form.get('senharequisitante')
 
+    with open('requisitante.json') as requisitante_json:
+        listaDeUsuarios = json.load(requisitante_json)
+            
+    user = [
+        {   
+            "email": email,
+            "nome": nome,
+            "senha": senha,
+        }
+    ]
 
+    novalista = listaDeUsuarios + user
+
+    with open("requisitante.json", 'w') as requisitante_json:
+        json.dump(novalista, requisitante_json, indent=4)
+
+    return redirect('/loginrequisitante')
 
 
 
